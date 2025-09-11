0a009baa89caed75bdd7badad0ae9f422e542ab3
PriontoAbdullah <priontosoft@gmail.com>
2025-09-12 00:00:59 +0600

    fix: layout ui

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -229,7 +229,7 @@ const Dashboard = () => {
         onLogout={handleLogout}
       />
       <div className='max-w-8xl mx-auto px-4 py-6'>
-        <div className='grid grid-cols-1 lg:grid-cols-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-6'>
           <div className='lg:col-span-3'>
             <LinkedInSidebar user={user} onLogout={handleLogout} />
           </div>
